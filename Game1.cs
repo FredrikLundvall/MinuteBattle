d8342258aef589b9f02541245911c35483abf49c
Fredrik Lundvall <lundvall_fredrik@hotmail.com>
2025-05-17 14:16:18 +0200

    First check in. Start of project with just some basic placeholder graphics

---
+++ Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MinuteBattle
+{
+    public class Game1 : Game
+    {
+        Texture2D spriteTexture;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        protected Rectangle drawingRectangle;
+        private float rotationAngle;
+        private Vector2 spritePosition;
+        private Vector2 spriteOrigin;
+        SpriteFont font;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+           // TODO: use this.Content to load your game content here
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteTexture = Content.Load<Texture2D>("Brittish/brittish_soldier_128");
+            font = Content.Load<SpriteFont>("GUI/fonts/BebasNeue-Regular");
+            // Get the viewport (window) dimensions
+            var viewport = _graphics.GraphicsDevice.Viewport;
+
+            // Set the sprite drawing area from the Viewport origin (0,0) to 80% the sprite scale width and 100% of the sprite scale height.
+            drawingRectangle.X = viewport.X;
+            drawingRectangle.Y = viewport.Y;
+            drawingRectangle.Width = 32;
+            drawingRectangle.Height = 32;
+
+            // Set the Texture origin to be the center of the texture.
+            spriteOrigin.X = 40; // spriteTexture.Width / 2;
+            spriteOrigin.Y = 77; // spriteTexture.Height / 2;
+
+            // Set the position of the texture to be the center of the screen.
+            spritePosition.X = viewport.Width / 2;
+            spritePosition.Y = viewport.Height / 2;
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            // The time since Update was called last.
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            // TODO: Add your game logic here.
+            // Simple roation logic to rotate the sprite in a clockwise direction over time
+            rotationAngle += elapsed;
+            float circle = MathHelper.Pi * 2;
+            rotationAngle %= circle;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            //_spriteBatch.Draw(soldierSprite, spritePosition, Color.White);
+            //_spriteBatch.Draw(soldierSprite, drawingRectangle, Color.White);
+            _spriteBatch.Draw(spriteTexture, spritePosition, null, Color.White, rotationAngle, spriteOrigin, 0.5f, SpriteEffects.None, 0f);
+
+            // Finds the center of the string in coordinates inside the text rectangle
+            var text = "Minute Battle";
+            Vector2 textMiddlePoint = font.MeasureString(text) / 2;
+            // Places text in center of the screen
+            Vector2 position = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 4);
+            _spriteBatch.DrawString(font, text, position, Color.DarkOliveGreen, 0, textMiddlePoint, 1.0f, SpriteEffects.None, 0.5f);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new MinuteBattle.Game1();
+game.Run();
