6e3355c952e7d760c1863e928c5b54cff536d67b
Fredrik Lundvall <lundvall_fredrik@hotmail.com>
2025-12-06 21:07:10 +0100

    Added signals for hovering over units in battle. Added different collisions for the physics and the mouse clicks

--- GameSource/Content/godot/Battle/battle.gd
+++ GameSource/Content/godot/Battle/battle.gd
@@ -1,8 +1,9 @@
 class_name Battle extends Node2D
 
-
 @onready var terrain: TileMapLayer = $Background/Terrain
 @onready var meleePicture: Texture2D = preload("res://hero/melee.png")
+
+signal unit_selected(unit: Unit)
 
 const SPAWN_COORDINATES = Vector2i(0,3)
 const SPAWN_ATLAS_TILE = Vector2i(0,0)
@@ -17,3 +18,23 @@ func spawn_unit(unit: Unit):
 	var rng = RandomNumberGenerator.new()
 	unit.position += Vector2(rng.randf_range(-10.0, 10.0),rng.randf_range(-10.0, 10.0))
 	terrain.add_child(unit)
+	
+func _on_unit_hovered(unit: Unit) -> void:
+	unit.highlighted = true
+
+func _on_unit_unhovered(unit: Unit) -> void:
+	unit.highlighted = false
+	
+func _on_unit_clicked(unit: Unit) -> void:
+	unit_selected.emit(unit)
+
+func unit_connect(unit: Unit):
+	if !unit.unit_hovered.is_connected(_on_unit_hovered):   
+		unit.unit_hovered.connect(_on_unit_hovered)
+	if !unit.unit_unhovered.is_connected(_on_unit_unhovered):   
+		unit.unit_unhovered.connect(_on_unit_unhovered)
+	if !unit.unit_clicked.is_connected(_on_unit_clicked):   
+		unit.unit_clicked.connect(_on_unit_clicked)
+
+func _on_terrain_child_entered_tree(node: Node) -> void:
+	unit_connect(node as Unit)
--- GameSource/Content/godot/unit/unit.gd
+++ GameSource/Content/godot/unit/unit.gd
@@ -1,14 +1,16 @@
 class_name Unit extends Node2D
 
 @export var picture: Texture2D = preload("res://hero/melee.png")
+@export var hovered: bool = false
 @export var highlighted: bool = false
 
-signal unit_clicked(card: Unit)
+signal unit_hovered(unit: Unit)
+signal unit_unhovered(unit: Unit)
+signal unit_clicked(unit: Unit)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var pictureSpr: Sprite2D = $Picture
-	pictureSpr.texture = picture
+	$Picture.texture = picture
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -26,3 +28,11 @@ func unhighlight():
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("mouse_click"):
 		unit_clicked.emit(self)
+
+func _on_mouse_entered() -> void:
+	hovered = true
+	unit_hovered.emit(self)
+
+func _on_mouse_exited() -> void:
+	hovered = false
+	unit_unhovered.emit(self)
